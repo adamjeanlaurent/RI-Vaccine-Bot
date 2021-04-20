34d4227b6fe25fd0cad34ce4f36c0cbdd79febf8
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-04-19 23:40:48 -0400

    beginnigs of web scraper

---
+++ bot/src/VaccineAppointment.js
@@ -0,0 +1,9 @@
+class VaccineAppointment {
+    constructor(time, date, location) {
+        this.time = time,
+        this.date = date;
+        this.location = location;
+    }
+}
+
+module.exports = VaccineAppointment;
