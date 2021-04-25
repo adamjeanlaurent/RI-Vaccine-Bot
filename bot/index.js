14d623d9a3964513f528721c6da00bcacc5ac707
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-04-25 16:11:07 -0400

    texting done woo

--- bot/index.js
+++ bot/index.js
@@ -2,9 +2,11 @@ const VaccineBot = require('./src/VaccineBot');
 
 const main = async () => {
     const vaccineBot = new VaccineBot();
+    const SECOND = 1000;
+    const MINUTE = SECOND * 60;
     while(true) {
         await vaccineBot.run();
-        await vaccineBot.sleep(20 * 1000); // sleep for 20 minutes
+        await vaccineBot.sleep(20 * MINUTE); // sleep for 20 minutes
     }
 }
 
