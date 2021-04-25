554496fdfa4340d0ac0e5c38bbb3e1790862667f
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-04-25 12:33:35 -0400

    unit test for vaccine bot

--- bot/src/test-VaccineBot.js
+++ bot/src/test-VaccineBot.js
@@ -61,3 +61,34 @@ const getMockAppointments = () => {
         }
     ];
 }
+
+const testProcessTaskQueue = async () => {
+    let availableAppointments = getMockAppointments();
+
+    let appointmentsMap = new Map();
+
+    for(let app of availableAppointments) {
+        if(appointmentsMap.has(app.date)) {
+            appointmentsMap.get(app.date).push(app);
+        }
+        else {
+            appointmentsMap.set(app.date, [ app ]);
+        }
+    }
+
+    let vaccineBot = new VaccineBot();
+
+    const taskQueue = await vaccineBot.getUncompletedTasks();
+
+    const matchingAppointments = await matchingAppointments.processTaskQueue(taskQueue, appointmentsMap);
+
+    assert.strictEqual(matchingAppointments.length, 1, 'there should be 1 valid matching appointment');
+    assert.strictEqual(matchingAppointments[0].time, availableAppointments[0].time, 'appointment time should be 11:10 am');
+    assert.strictEqual(matchingAppointments[0].location, availableAppointments[0].location, 'location should be East Providence POD');
+    assert.strictEqual(matchingAppointments[0].link, availableAppointments[0].link, 'link should be https://www.vaccinateri.org/client/registration?clinic_id=2055');
+    assert.strictEqual(matchingAppointments[0].date, availableAppointments[0].date, 'date should be 04/23/2021');
+    assert.strictEqual(matchingAppointments[0].firstname, 'john', 'first name should be john');
+    assert.strictEqual(matchingAppointments[0].lastname, 'doe', 'last name should be doe');
+}
+
+testProcessTaskQueue();
