3fda8c746b521315fd0d8b0d2c61304006a11c36
Phidias <mendezphidias@gmail.com>
2021-04-27 11:06:34 -0400

    /api/getTasks - Returns all tasks for a user

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.post('/api/authenticate', function(req, res) {
 
 // POST - Add A Task To The Bot
 app.post('/api/addTask', function(req, res){
-    if (req.session){
+    if (typeof req.session.userID !== "undefined"){
         // Gather userID
         let userID = req.session.userID;
         let f_name = req.body.f_name;
@@ -121,12 +121,58 @@ app.post('/api/addTask', function(req, res){
         let end_time = req.body.end_time;
 
         let sql = `INSERT INTO task (userID, f_name, l_name, phone, date_picked, start_time, end_time, completed) VALUES ("${userID}", "${f_name}", "${l_name}", "${phone}", "${date_picked}", "${start_time}", "${end_time}", "false")`;
+        console.log(sql);
 
         conn.query(sql, function (err, rows, fields){
             if (err) throw err;
             return res.send("Task just added. We will see when the task is completed!");
         });
-    }
+    } else {return res.send("No session detected.");}
+});
+
+// GET - List My Tasks
+app.get('/api/getTasks', async function(req, res){
+    // Make sure user is signed in.
+    if(typeof req.session.userID !== "undefined"){
+        //Grab userID of session
+        let userID = req.session.userID;
+
+        let sql = `SELECT * FROM task WHERE userID = ${userID};`;
+
+        let userPromise = new Promise(function(myResolve, myReject){
+            let jsonReturn = new Array();
+            console.log(sql);
+            let dbCallback = function(callback){
+                conn.query(sql, function (err, rows, fields){
+                    if (err) throw err;
+                    if (rows.length == 0){return res.send("No tasks for this user.");}
+                    console.log("dbPromise");
+                    for(var i = 0; i < rows.length; i++){
+                        console.log(i);
+                        let json = {
+                            "TaskID": rows[i].taskID,
+                            "FirstName": rows[i].f_name,
+                            "LastName": rows[i].l_name,
+                            "Phone": rows[i].phone,
+                            "Date": rows[i].date_picked,
+                            "StartTime": rows[i].start_time,
+                            "EndTime": rows[i].end_time,
+                            "Completed": rows[i].completed
+                        };
+                        jsonReturn.push(json);
+                    }
+                    callback(null, jsonReturn);
+                });
+            };
+            dbCallback(function(err, jsonReturn){
+                if (err) console.log("Database error!");
+                else {console.log(jsonReturn); myResolve(jsonReturn);}
+            });
+        });
+        
+        let jsonData = await userPromise;
+        return res.send(jsonData);
+    } else {return res.send("No session detected.");}
 });
 
 // Returns any requests to the index.html
