d4d18799bd22275f3efbe21405e814ccb6aeb988
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-04-30 10:26:43 -0400

    concurrent

--- client/src/constants/apiConstants.js
+++ client/src/constants/apiConstants.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
+// export const API_BASE_URL = 'http://localhost:5000';
 export const API_BASE_URL = 'http://localhost:5000';
 export const ACCESS_TOKEN_NAME = 'login_access_token';
