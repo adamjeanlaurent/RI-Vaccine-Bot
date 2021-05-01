519c2c2384c3ca2aacf4dc24e5e38f3acca8cee2
Adam Jean-Laurent <adamjeanlaurent@gmail.com>
2021-05-01 11:10:02 -0400

    formatted email and basic bot error handling

--- bot/index.js
+++ bot/index.js
@@ -1,12 +1,21 @@
 const VaccineBot = require('./src/VaccineBot');
+const colors = require('colors');
 
 const main = async () => {
     const vaccineBot = new VaccineBot();
     const SECOND = 1000;
     const MINUTE = SECOND * 60;
     while(true) {
-        await vaccineBot.run();
-        await vaccineBot.sleep(20 * MINUTE); // sleep for 20 minutes
+        try {
+            await vaccineBot.run();
+            await vaccineBot.sleep(20 * MINUTE); // sleep for 20 minutes
+        }
+        catch(error) { 
+            // log error
+            // sleep for a bit to avoid spam
+            console.log(colors.red(error.message));
+            await vaccineBot.sleep(5 * MINUTE);
+        }
     }
 }
 
--- bot/src/email.js
+++ bot/src/email.js
@@ -3,27 +3,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sendEmail = (to, body) => {
-  const senderMail = process.env.EMAIL_USER;
-  const senderPass = process.env.EMAIL_APP_CODE;
+    const senderMail = process.env.EMAIL_USER;
+    const senderPass = process.env.EMAIL_APP_CODE;
 
-  const emailTransporter = nodemailer.createTransport({
-    host: 'smtp.mail.yahoo.com',
-    port: 465,
-    service:'yahoo',
-    secure: false,
-    auth: {
-      user: senderMail,
-      pass: senderPass
-    },
-    debug: false,
-  });
+    const emailTransporter = nodemailer.createTransport({
+        host: 'smtp.mail.yahoo.com',
+        port: 465,
+        service:'yahoo',
+        secure: false,
+        auth: {
+            user: senderMail,
+            pass: senderPass
+        },
+        debug: false,
+    });
 
-    body = body.replace('\n', '<br>');
     // send mail with defined transport object
     emailTransporter.sendMail({
         from: '"Vaccine Bot" <rivaccinebot@yahoo.com>', // sender address
         to: to, // list of receivers
-        subject: "Vaccine Appointment Found For You 😇", // Subject line
+        subject: "Vaccine Appointment Found For You 💉", // Subject line
         html: body, // html body
     });
 }
